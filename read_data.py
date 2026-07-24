954be37d5b73678ae1f49fc88ec86d785fe5fac2
nimishh22 <nimish.k2010@gmail.com>
2026-07-24 19:08:18 +0530

    Add files via upload
    implemented feature engineering by adding recent form feature

--- read_data.py
+++ read_data.py
@@ -20,9 +20,9 @@ df_filtered = df[df["tournament"].isin(important)].copy()
 df_filtered["date"] = pd.to_datetime(df_filtered["date"])
 df_filtered = df_filtered[df_filtered["date"].dt.year >= 2014]
 df_filtered = df_filtered.sort_values("date")
-print(df_filtered.shape)
-print(df_filtered.head(10))
-print(df_filtered["tournament"].value_counts())
+#print(df_filtered.shape)
+#print(df_filtered.head(10))
+#print(df_filtered["tournament"].value_counts())
 
 def get_result(row):
     if row["home_score"] > row["away_score"]:
@@ -33,7 +33,7 @@ def get_result(row):
         return 0
     
 df_filtered["result"] = df_filtered.apply(get_result, axis=1)
-print(df_filtered[["home_team", "away_team", "home_score", "away_score", "result"]].head(10))
+#print(df_filtered[["home_team", "away_team", "home_score", "away_score", "result"]].head(10))
 
 
 def get_recent_form(team, match_date):
@@ -45,9 +45,41 @@ def get_recent_form(team, match_date):
         )
         & (df_filtered["date"] < match_date)
     ]
-    print(len(team_matches))
+    #print(len(team_matches))
     recent_matches = team_matches.tail(10)
     points = 0
-    print (recent_matches[["date", "home_team", "away_team", "result"]])
-get_recent_form("Brazil", "2022-11-23")
 
+    for _, row in recent_matches.iterrows():
+        if row["home_team"] ==  team:
+            team_score = row["home_score"]
+            opponent_score = row["away_score"]
+        else:
+            team_score = row["away_score"]
+            opponent_score = row["home_score"]
+
+        if team_score > opponent_score:
+            points += 3
+        elif team_score == opponent_score:
+            points += 1
+    return points / 30 
+
+home_forms = []
+away_forms = []
+
+for _, row in  df_filtered.iterrows():
+    home_form = get_recent_form(
+        row["home_team"] , 
+        row["date"] 
+    )
+    away_form = get_recent_form(
+        row["away_team"] ,
+        row["date"]
+    ) 
+
+    home_forms.append(home_form)
+    away_forms.append(away_form)
+
+df_filtered["home_form"] = home_forms
+df_filtered["away_form"] = away_forms
+
+print(df_filtered.iloc[3000:3005]) 
